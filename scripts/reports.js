a4c53247a48e82886007d23981fce6acc1d77205
ajt <48979824+ankitjt@users.noreply.github.com>
2022-08-04 16:23:15 +0530

    Add files via upload

--- scripts/reports.js
+++ scripts/reports.js
@@ -2,20 +2,25 @@ let sectionButton = document.querySelectorAll(".sectionButton"),
     content = document.querySelector(".content"),
     downArrow = document.querySelector(".downArrow")
 
-// sectionButton.onclick = () => {
-//     content.classList.toggle("hidden")
-//     downArrow.classList.toggle("rotate-180")
-// }
+// Display reports by name 
 
 for(let thisSection of sectionButton) {
     thisSection.onclick = () => {
-        downArrow.classList.toggle("rotate-180")
-        var panel = thisSection.nextElementSibling
+        let thisSectionParent = thisSection.parentElement
+        let sectionButtonWrapper = thisSection.childNodes[1];
+        let downArrowChild = thisSection.childNodes[1].childNodes[3]
+        let panel = thisSection.nextElementSibling
+
+        downArrowChild.classList.toggle("rotate-180")
         if(panel.style.display === 'block') {
             panel.style.display = 'none'
+            sectionButtonWrapper.classList.remove("border-b-2")
+            thisSectionParent.classList.add("lg:hover:scale-95", "lg:transition-all", "lg:ease-in-out")
         }
         else {
+            sectionButtonWrapper.classList.add("border-b-2")
             panel.style.display = "block"
+            thisSectionParent.classList.remove("lg:hover:scale-95", "lg:transition-all", "lg:ease-in-out")
         }
        
     }
