da4689b1998e9eac3fd737a80af3fc6273031139
ajt <48979824+ankitjt@users.noreply.github.com>
2022-12-29 16:08:41 +0530

    Add files via upload

--- scripts/commonScripts/patientList.js
+++ scripts/commonScripts/patientList.js
@@ -1,6 +1,11 @@
-let patientList = document.querySelector( '.patientList' );
+let patientList = document.querySelector( '.patientList' ),
+  patientListButton = document.querySelector( '.patientListButton' ),
+  patientEmail = document.querySelector( '.patientEmail12' ),
+  patientName = document.querySelector( '.patientName12' );
+
 ( () =>
 {
+
   db.collection( "profiles" ).onSnapshot( ( querySnapshot ) =>
   {
     let allNames = document.querySelectorAll( '.something' )
@@ -14,15 +19,39 @@ let patientList = document.querySelector( '.patientList' );
       if ( doc.data().softDelete !== true )
       {
         let patientNames = `
-                
-                <option value="${ doc.data().aptName }" class="md:font-semibold md:flex md:items-center md:justifiy-between w-full h-auto text-xs my-10" data-id="${ doc.id }" >
-                <div>${ doc.data().aptName } ${doc.data().aptType === 'Session' ? '(S)' : '(SV)' } </div> &nbsp;
-                <div class="text-red-500 lowercase ml-10">( ${ doc.data().aptEmail } )</div> &nbsp;
-                </option>
-                <br />
-                `
-        patientList.innerHTML += patientNames
+                <li
+                    class="text-gray-900 border-b-2 font-semibold border-gray-200 hover:bg-blue-100 ease-in-out duration-300 cursor-pointer patientListItems relative oveflow-x-hidden cursor-default py-2 w-full"
+                    id="listbox-option-0" role="option" data-id="${ doc.id } ">
+                    <div class="flex items-center lg:flex-row justify-between px-2 md:flex-col">
+                      <div class='patientListWrapper items-center flex text-xs'>
+                      <span class="font-semibold block truncate optionName tracking-normal">${ doc.data().aptName }</span>
+                      <div class="text-blue-500 text-xs ml-2 lowercase ">(${ doc.data().aptEmail })</div>
+                        <span class="ml-2 bg-red-400 rounded-full px-2 py-1 font-normal lowercase text-slate-50">${ doc.data().aptType === 'Session' ? 'S' : 'SV' }</span>
+                      </div>
+                    </div>
+                  </li>
+                  `
+                  patientList.innerHTML += patientNames
+                  selectingPatientName()
       }
     } )
   } )
+
 } )()
+
+const selectingPatientName = () =>
+{
+  patientListButton.onclick = () =>
+  {
+    patientList.classList.toggle( 'hidden' )
+    let patientListItems = document.querySelectorAll( '.patientListItems' )
+    for ( let item of patientListItems )
+    {
+      item.onclick = () =>
+      {
+        patientName.innerText = item.childNodes[ 1 ].childNodes[ 1 ].childNodes[ 1 ].innerText
+        patientList.classList.toggle( 'hidden' )
+      }
+    }
+  }
+}
