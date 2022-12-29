e3ba6b19a78ce0da3ccafea6abe5203ad1b64584
ajt <48979824+ankitjt@users.noreply.github.com>
2022-12-29 16:10:36 +0530

    Add files via upload

--- scripts/schedule/schedule_showingApts.js
+++ scripts/schedule/schedule_showingApts.js
@@ -2,26 +2,16 @@
 const showingApts = ( doc, aptStartDate, aptStartDateMonth ) =>
 {
   let myData = new Date( doc.data().statusUpdatedTimeStamp.seconds * 1000 )
-  let currentMonthAppointments = /*html*/ `
-              <tr class="border-l-2 border-b-2 text-[11px] tableRow12 border-r-2 border-gray-200" data-id="${ doc.id
-    }">
-                <td class="py-3 px-5 font-semibold">
-                  ${ doc.data().aptName }
-                </td>
-                <td class="py-3 px-5 font-semibold">
-                  ${ aptStartDate.getDate() }-${ aptStartDateMonth }-${ aptStartDate.getFullYear() }
-                </td>
-                <td class="py-3 px-5 font-semibold">
-                  ${ doc.data().aptDay } <br /> ${ doc.data().aptTimeSlot }
-                </td>
-                <td class="py-3 px-5 font-semibold">
-                  ${ doc.data().aptType }
-                </td>
-              
-                <!-- Status -->
-                <td class="py-3   px-5 font-semibold">
-              
-                  <div class="appointmentStatus">
+  let currentMonthAppointments = `
+  <div class="flex flex-col">
+              <div class="flex items-center text-center text-xs border-t-2 border-gray-200 py-3 hover:bg-blue-100 ease-in-out duration-300 text-blue-500 font-semibold" data-id="${ doc.id }">
+              <span class="w-[180px]">${ doc.data().aptName }</span>
+        
+              <span class="w-[180px] ">${ doc.data().aptTimeSlot }<br />
+              ${ aptStartDate.getDate() }-${ aptStartDateMonth }-${ aptStartDate.getFullYear() }, <span class=''> ${ doc.data().aptDay} </span> </span>
+              <span class="w-[180px]">${ doc.data().aptType }</span>
+              <span class="w-[180px]">
+                <div class="appointmentStatus uppercase text-xs">
                     <div class="${ doc.data().appointmentStatus === "Completed" ? "block" : "hidden" }">
                       <span class="text-emerald-500">
                         ${ doc.data().appointmentStatus === undefined ? "Scheduled" : doc.data().appointmentStatus }
@@ -38,7 +28,7 @@ const showingApts = ( doc, aptStartDate, aptStartDateMonth ) =>
                       </span>
                     </div>
                     <div class="${ doc.data().appointmentStatus === "Pending" ? "block" : "hidden" }">
-                      <span class="text-amber-500">
+                      <span class="text-amber-600">
                         ${ doc.data().appointmentStatus === undefined ? "Scheduled" : doc.data().appointmentStatus }
                       </span>
                     </div>
@@ -50,38 +40,27 @@ const showingApts = ( doc, aptStartDate, aptStartDateMonth ) =>
                     <span class='text-xs text-orange-500'>${ doc.data().softDelete === true ? 'Profile Deleted' : '' } </span>
                   </div>
               
-                  <div class="statusUpdateTime">
-                    <span class="text-[10px] text-gray-900"> ${ doc.data().statusUpdatedTimeStamp === "" ? '' : myData.toDateString()
+               <div class="statusUpdateTime">
+                    <span class="text-[10px] font-semibold"> ${ doc.data().statusUpdatedTimeStamp === "" ? '' : myData.toDateString()
       + " " + myData.toLocaleTimeString() } </span>
-                  </div>
-              
-                </td>
-              
-                <td class="py-3  px-5 font-semibold">
-                  <select name="appointmentActions" id="appointmentActions"
-                    class="border-gray-200 border-2 rounded-lg w-full placeholder:text-blue-900 font-medium lg:placeholder:text-sm py-2  aptActions lg:drop-shadow-none drop-shadow-2xl text-xs">
-                    <option value="Action" class="font-semibold">
-                      Action
-                    </option>
-                    <option value="Completed" class="font-semibold">
-                      Completed
-                    </option>
-                    <option value="Paid Cancelled" class="font-semibold">
-                      Paid Cancelled
-                    </option>
-                    <option value="Free Cancelled" class="font-semibold">
-                      Free Cancelled
-                    </option>
-                    <option value="Updated" class="font-semibold">
-                      Edit/Update
-                    </option>
-                    <option value="Close" class="font-semibold">
-                      Closed
-                    </option>
-                  </select>
-                </td>
-              </tr>
-              `
+                  </div> 
+              </span>
+              <span class="w-[180px] text-center">
+                <select id="countries"
+                  class="aptActions border border-blue-500 text-blue-500 text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block  px-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                  <option selected>Select</option>
+                  <option value="US" class='font-semibold text-gray-900'>Completed</option>
+                  <option value="CA" class='font-semibold text-gray-900'>Pending</option>
+                  <option value="FR" class='font-semibold text-gray-900'>Free Cancelled</option>
+                  <option value="DE" class='font-semibold text-gray-900'>Paid Cancelled</option>
+                  <option value="DE" class='font-semibold text-gray-900'>Edit/Update</option>
+                  <option value="DE" class='font-semibold text-gray-900'>Closed</option>
+                </select>
+              </span>
+            </div>
+
+            </div>
+  `
   scheduleTableRows.innerHTML += currentMonthAppointments
 
 }
