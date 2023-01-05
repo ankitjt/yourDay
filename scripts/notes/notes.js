a7c8e883f9e942e30de6e7a74f2ea2cb6f59e320
ajt <48979824+ankitjt@users.noreply.github.com>
2023-01-05 15:32:55 +0530

    Add files via upload

--- scripts/notes/notes.js
+++ scripts/notes/notes.js
@@ -1,19 +1,14 @@
 const storage = firebase.storage()
 let bannerHolder = document.querySelector( '.banner-holder' )
+let notesOfPatients
+let uploadedNotesList = document.querySelector( '.uploadedNotesList' )
+let uploadNoteFormWrapper = document.querySelector( '.uploadNoteFormWrapper' )
 
 // Listing All files for selected name.
-bannerHolder.onclick = () =>
-{
-  patientList.classList.add('hidden')
-}
-
 patientList.onclick = () =>
 {
-
-  let uploadedNotesList = document.querySelector( '.uploadedNotesList' )
   let uploadedNotes = document.querySelector( '.uploadedNotes' )
   let notesCount = document.querySelector( '.notesCount' )
-  let uploadNoteFormWrapper = document.querySelector( '.uploadNoteFormWrapper' )
 
   if ( patientListButton.innerText === 'By Name' )
   {
@@ -23,10 +18,11 @@ patientList.onclick = () =>
   else
   {
     let myArr = []
-    uploadNoteFormWrapper.classList.remove( 'hidden' )
+
     uploadedNotesList.innerHTML = ''
     notesCount.innerHTML = ''
-    let listRef = firebase.storage().ref( `ptNotes/${ patientName.innerText }/` )
+    let listRef = firebase.storage().ref( `ptNotes/${ patientName.getAttribute( "data-id" ) }/` )
+    uploadNoteFormWrapper.classList.remove( 'hidden' )
 
     let s = firebase.storage().ref( 'ptNotes' )
     let dataSort = []
@@ -34,7 +30,7 @@ patientList.onclick = () =>
     {
       res.prefixes.forEach( ( folderRef ) =>
       {
-        if ( folderRef.name === patientName.innerText )
+        if ( folderRef.name === patientName.getAttribute( "data-id" ) )
         {
           listRef.listAll().then( ( res ) =>
           {
@@ -51,43 +47,49 @@ patientList.onclick = () =>
                     return a - b
                   } )
                   let uploadDate = new Date( metadata.updated )
-                  let notesOfPatients =
-                  /*html*/ `
-                    <div class="note mb-4 flex items-center justify-between lg:hover:bg-gray-200 p-2 rounded-lg lg:transition-all lg:ease-in-out">
-                      <div class="noteLink flex flex-col">
-                          <a href="${ url }" target='_blank' class="text-blue-600" download>
-                          <span class="noteName font-semibold underline">${ itemRef.name }</span>
-                          </a>
-                          <span class="noteUploadTime text-xs font-semibold text-gray-400 tracking-wider mt-1">
-                          ${ uploadDate.getDate() }/${ uploadDate.getMonth() + 1 }/${ uploadDate.getFullYear() }, ${ uploadDate.getHours() }:${ uploadDate.getMinutes() }:${ uploadDate.getSeconds() }
-                          </span>
-                      </div>
-                      <div class="noteDelete">
-                          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-rose-600 cursor-pointer" fill="none"
-                          viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                          <path stroke-linecap="round" stroke-linejoin="round"
-                              d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                          </svg>
-                      </div>
-                  </div>
-                    `
+                  myArr.push( itemRef )
+                  notesOfPatients =
+                    /*html*/ `
+                      <div class="note mb-4 flex items-center justify-between hover:bg-red-100 md:hover:bg-gray-200 p-3 rounded-lg transition-all ease-in-out duration-300">
+                        <div class="noteLink flex flex-col">
+                            <a href="${ url }" target='_blank' class="text-blue-600" download>
+                            <span class="noteName font-semibold underline">${ itemRef.name }</span>
+                            </a>
+                            <span class="noteUploadTime text-xs font-semibold text-gray-400 tracking-wider mt-1">
+                            ${ uploadDate.getDate() }/${ uploadDate.getMonth() + 1 }/${ uploadDate.getFullYear() }, ${ uploadDate.getHours() }:${ uploadDate.getMinutes() }:${ uploadDate.getSeconds() }
+                            </span>
+                        </div>
+                        <div class="noteDelete">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-rose-600 cursor-pointer" fill="none"
+                            viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                            </svg>
+                        </div>
+                    </div>
+                      `
                   uploadedNotesList.innerHTML += notesOfPatients
                   uploadedNotes.classList.remove( 'hidden' )
+                  notesCount.innerHTML = ` ( ${ myArr.length } ) `
                 } )
               } )
-
-              myArr.push( itemRef )
-              notesCount.innerHTML = ` ( ${ myArr.length } ) `
-
             } )
           } )
         }
 
-
+        else
+        {
+          uploadedNotesList.innerHTML = `
+            <div class="emptyState h-full w-full p-12 flex flex-col items-center" >
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-12 h-12 text-red-500 my-5">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
+              </svg>
+              <h3 class="text-center font-semibold text-red-500">No notes found.</h3>
+            </div>
+          `
+        }
       } )
     } )
-
-
   }
 }
 
@@ -102,23 +104,45 @@ uploadNoteBtn.onclick = ( e ) =>
 
   if ( patientListButton.innerText === 'By Name' )
   {
-    promptMessages( 'Select patient name first...' )
+    promptMessages( 'Select patient name first.' )
   }
 
   else if ( file === undefined )
   {
-    promptMessages( 'upload a file....' )
+    promptMessages( 'Browse a file to upload.' )
+  }
+
+  else if ( /\.(jpe?g|pdf)$/i.test( file.name ) !== true )
+  {
+    promptMessages( 'File should be in .pdf or .jpg format only.' )
   }
 
   else
   {
+    // Check for same file name to be added.
+    // let checkFileName = firebase.storage().ref( `ptNotes/${ patientName.getAttribute( 'data-id' ) }/${ file.name }` )
+    // let storageRef = firebase.storage().ref( `ptNotes/${ patientName.getAttribute( 'data-id' ) }/` + file.name )
+    // console.log( checkFileName, storageRef );
+    let storageRef = firebase.storage().ref( `ptNotes/${ patientName.getAttribute( 'data-id' ) }/` + file.name )
     let metadata = {
       uploadTime: firebase.firestore.FieldValue.serverTimestamp()
     }
-    let storageRef = firebase.storage().ref( `ptNotes/${ patientName.innerText }/` + file.name )
+
     storageRef.put( file, metadata )
-    promptMessages( 'file uploaded' )
+    promptMessages( 'File uploaded successfully.' )
     fileUpload.value = ''
+
+    // if ( checkFileName === storageRef)
+    // {
+    //   promptMessages( `A note with same name exists for the patient. 
+    //    Please check exisiting notes of the patient or give a different file name.`)
+    // }
+    // else
+    // {
+      
+      
+
+    // }
   }
 
 }
