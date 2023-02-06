04fad4b0c9aa1dd138ae2f95011912089a7de8b4
ajt <48979824+ankitjt@users.noreply.github.com>
2023-02-06 16:29:44 +0530

    Add files via upload

--- scripts/commonScripts/commonVariables.js
+++ scripts/commonScripts/commonVariables.js
@@ -3,6 +3,7 @@ let pageWrapper = document.querySelector( '.pageWrapper' )
 let reportsWrapper = document.querySelector( '.reportsWrapper' )
 let promptsWrapper = document.querySelector( '.promptsWrapper' )
 let pb = document.querySelector( '.pb' )
+let startDate = document.querySelector(".aptStartDate")
 let rightSection = document.querySelector( '.rightSection' )
 let countryCode = document.querySelectorAll( ".countryCode" )
 let dataArr = []
--- scripts/commonScripts/fetchingData.js
+++ scripts/commonScripts/fetchingData.js
@@ -6,13 +6,13 @@ const fetchingData = () =>
     {
       querySnapshot.forEach( ( doc ) =>
       {
-        let aptStartDate = new Date( doc.data().dateInMills.at( -1 ) * 1000 )
+        let appointmentStartDate = new Date( doc.data().dateInMills.at( -1 ) * 1000 )
         lastElementOfUpdatedStatus = doc.data().statusUpdatedTimeStamp.at( -1 )
         lastUpdatedDate = new Date( lastElementOfUpdatedStatus.seconds * 1000 )
         let currentDate = new Date()
         let currentMonth = currentDate.getMonth()
         let lastMonth = currentDate.getMonth() - 1
-        let aptStartDateMonth = aptStartDate.getMonth()
+        let aptStartDateMonth = appointmentStartDate.getMonth()
 
         dataArr.push( {
           type: doc.data().aptType,
@@ -20,7 +20,7 @@ const fetchingData = () =>
           day: doc.data().aptDay.at( -1 ),
           name: doc.data().aptName,
           status: doc.data().appointmentStatus,
-          convertedDate: aptStartDate,
+          convertedDate: appointmentStartDate,
           id: doc.id,
           email: doc.data().aptEmail,
           date: doc.data().aptStartDate.at( -1 ),
@@ -29,6 +29,7 @@ const fetchingData = () =>
           showUpdate: doc.data().showUpdate
         } )
       } )
+     
     } )
   }
 }
