a87399acdf55db070fd5846a0e2cb0dd947f0a62
ajt <48979824+ankitjt@users.noreply.github.com>
2023-02-06 16:30:19 +0530

    Add files via upload

--- scripts/schedule/addMoreAppointments.js
+++ scripts/schedule/addMoreAppointments.js
@@ -1,14 +1,92 @@
-const addMoreAppointments = () =>
+const addMoreAppointments = (  ) =>
 {
     let showUpdate = document.querySelectorAll( '.showUpdate' )
-
     for ( let update of showUpdate )
     {
         update.onclick = () =>
         {
-            let ptName = update.parentElement.childNodes[ 3 ].innerText  
-            db.collection(`appointments/${ptName}/details`)
+            
+            let currentUserData = [];
+            let ptName = update.parentElement.childNodes[ 3 ].innerText
+            console.log(dataArr)
+            for ( let userDetails of dataArr )
+            {
+                if ( ptName === userDetails.email )
+                {
+                    currentUserData.push( userDetails )
+                }
+            }
+            console.log( currentUserData )
+         
+            let lastEntry = currentUserData.at( -1 )
+            // console.log(lastEntry)
+
+            let dbEntry = db.collection( `appointments/${ lastEntry.email }/details` )
+
+            let newDate = lastEntry.convertedDate
+            let firstDay = newDate.getDate().toString()
+            let firstMonth = ( newDate.getMonth() + 1 ).toString()
+            let firstYear = newDate.getFullYear().toString()
+            let appointmentDate = [ firstDay ]
+            let appointmentMonth = [ firstMonth ]
+            let appointmentYear = [ firstYear ]
+            let newDateInSec = newDate / 1000
+            let uppercaseName = lastEntry.name.toUpperCase()
+            let dateInMills = [ newDateInSec ]
+            let convertFees = parseInt( lastEntry.fees )
+
+            let addMoreCount = [ 1, 2, 3, 4, 5 ]
+
+            for ( let i = 0; i < addMoreCount.length; i++ )
+            {
+
+                // Getting future Date, Month, Year .
+                let futureAppointments = Math.floor( newDate.setDate( newDate.getDate() + 7 ) / 1000 )
+                dateInMills.push( futureAppointments )
+                let some = futureAppointments
+                let someTimes = new Date( some * 1000 )
+
+                let futureDate = someTimes.getDate().toString()
+                let finalFutureDate
+                futureDate < 10 ? finalFutureDate = '0' + futureDate : finalFutureDate = futureDate
+                appointmentDate.push( finalFutureDate )
+
+                let futureMonth = ( someTimes.getMonth() + 1 ).toString()
+                let finalFutureMonth
+                futureMonth < 10 ? finalFutureMonth = '0' + futureMonth : finalFutureMonth = futureMonth
+                appointmentMonth.push( finalFutureMonth )
+
+                let futureYear = someTimes.getFullYear().toString()
+                appointmentYear.push( futureYear )
+
+                let futureDates = `${ finalFutureDate }/${ finalFutureMonth }/${ futureYear }`
+                // console.log( futureDates )
+
+                // Creating Appointment for One Occurrence
+                // dbEntry.add( {
+                //     aptName: uppercaseName,
+                //     aptEmail: apt.email.value,
+                //     aptDay: [ apt.day.value ],
+                //     aptSecondDay: "NA",
+                //     aptTimeSlot: [ apt.timeSlot.value ],
+                //     aptSecondTimeSlot: "NA",
+                //     aptType: apt.type.value,
+                //     dateInMills: [ dateInMills[ i ] ],
+                //     aptStartDate: [ appointmentDate[ i ] ],
+                //     aptStartMonth: [ appointmentMonth[ i ] ],
+                //     aptStartYear: [ appointmentYear[ i ] ],
+                //     aptSecondStartDate: "NA",
+                //     appointmentStatus: 'Scheduled',
+                //     aptFees: convertFees,
+                //     serverTimeStamp: firebase.firestore.Timestamp.fromDate( new Date() ),
+                //     statusUpdatedTimeStamp: [ 'NA' ],
+                //     showUpdate: count[ i ] === 5 ? 'update' : ''
+                // } )
+
+            }
+           
         }
+        currentUserData = []
     }
 
 }
--- scripts/schedule/test_onLoad.js
+++ scripts/schedule/test_onLoad.js
@@ -1,3 +1,4 @@
+dataArr = []
 setTimeout( () =>
 {
   dataArr.sort( ( a, b ) =>
@@ -6,6 +7,7 @@ setTimeout( () =>
   } )
 
   scheduleGalleryView.innerHTML = ''
+  scheduleTableRows.innerHTML = ''
   for ( let eachRecord of dataArr )
   {
     let appointmentPill = `
@@ -57,5 +59,78 @@ setTimeout( () =>
            </div>
        `
     scheduleGalleryView.innerHTML += appointmentPill
+
+    // Desktop View
+    appointmentCount.innerText = `( ${ dataArr.length } )`
+    let currentMonthAppointments = `
+              <div class="flex flex-col justify-center align-middle tableRow12">
+                <div
+                  class="grid grid-cols-6 px-2 py-4 text-xs font-semibold text-center text-blue-600 duration-300 ease-in-out border-b border-gray-200 place-items-center hover:bg-blue-100"
+                  data-id="${ eachRecord.id }">
+                <span>
+                    <span class='scheduleName'>${ eachRecord.name } </span>
+                    <span class='scheduleEmail block text-[10px] text-gray-400 font-medium'>${ eachRecord.email } </span>
+                   <span class='showUpdate ${ eachRecord.showUpdate === 'update' ? 'inline-block' : 'hidden' } bg-rose-500 px-2 py-1 mt-1 text-white font-normal rounded-md uppercase text-[10px] cursor-pointer'>${ eachRecord.showUpdate } </span>
+                </span>
+                  <span>${ eachRecord.slot }</span>
+                  <span>
+                    <span> ${ eachRecord.day }, </span> <br />
+                    <span class='date'>${ eachRecord.date }-${ eachRecord.month }-${ eachRecord.year } </span>
+                  </span>
+                  <span>${ eachRecord.type }</span>
+                  <span>
+                    <div class="text-xs uppercase appointmentStatus">
+                      <div class="${ eachRecord.status === " Completed" ? "block" : "hidden" }">
+                        <span class="text-emerald-500">
+                          ${ eachRecord.status === undefined ? "Scheduled" : eachRecord.status }
+                        </span>
+                      </div>
+                      <div class="${ eachRecord.status === " Cancelled" ? "block" : "hidden" }">
+                        <span class="text-red-500">
+                          ${ eachRecord.status === undefined ? "Scheduled" : eachRecord.status }
+                        </span>
+                      </div>
+                      <div class="${ eachRecord.status === " Updated" ? "block" : "hidden" }">
+                        <span class="text-amber-500">
+                          ${ eachRecord.status === undefined ? "Scheduled" : eachRecord.status }
+                        </span>
+                      </div>
+                      <div class="${ eachRecord.status === " Pending" ? "block" : "hidden" }">
+                        <span class="text-amber-600 animate-ping">
+                          ${ eachRecord.status === undefined ? "Scheduled" : eachRecord.status }
+                        </span>
+                      </div>
+                      <div class="${ eachRecord.status === " Scheduled" ? "block" : "hidden" }">
+                        <span>
+                          ${ eachRecord.status === "Scheduled" ? "Scheduled" : eachRecord.status }
+                        </span>
+                      </div>
+                      <span class='text-xs text-orange-500'>${ eachRecord.profileStatus === true ? 'Profile Deleted' : '' } </span>
+                    </div>
+
+                    <div class="statusUpdateTime">
+                      <span class="text-[10px] font-semibold">
+                      ${ lastElementOfUpdatedStatus === 'NA' ? 'NA' : lastUpdatedDate.toLocaleDateString() + ',' + lastUpdatedDate.toLocaleTimeString() }
+                        </span>
+                    </div>
+                  </span>
+                  <span class="flex justify-center w-full ">
+                    <select
+                      class="block w-5/6 text-xs text-blue-500 border border-gray-300 rounded-lg aptActions focus:ring-blue-500 focus:border-blue-500">
+                      <option selected>Select</option>
+                      <option value="Completed" class='font-semibold text-gray-900'>Completed</option>
+                      <option value="Free Cancelled" class='font-semibold text-gray-900'>Free Cancelled</option>
+                      <option value="Paid Cancelled" class='font-semibold text-gray-900'>Paid Cancelled</option>
+                      <option value="Update" class='font-semibold text-gray-900'>Update</option>
+                      <option value="Closed" class='font-semibold text-gray-900'>Closed</option>
+                    </select>
+                  </span>
+                </div>
+              </div>
+            `
+    scheduleTableRows.innerHTML += currentMonthAppointments
+    addMoreAppointments()
   }
+
 }, 2000 )
+
