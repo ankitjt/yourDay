1bafa285dcee6e7789bb34e239e515b41f626058
ankitjt <ankitjeetthakur@gmail.com>
2023-02-27 22:13:13 +0530

    adding UI changes to index page

--- scripts/commonScripts/panicButton.js
+++ scripts/commonScripts/panicButton.js
@@ -1,18 +1,27 @@
-
-pb.onclick = () =>
+let body = document.getElementsByTagName( 'body' )[ 0 ];
+( () =>
 {
-  let currentTime = new Date()
-  let currentOffset = currentTime.getTimezoneOffset()
-  let ISTOffset = 330   // IST offset UTC +5:30 
-  let ISTTime = new Date( currentTime.getTime() + ( ISTOffset + currentOffset ) * 60000 )
+  let createPanicButton = document.createElement( 'span' )
+  createPanicButton.innerHTML = `<span
+        class="absolute z-50 p-3 text-2xl font-medium text-white uppercase bg-rose-400 rounded-full cursor-pointer pb animate-bounce bottom-6 app-name right-12 lg:left-10 lg:right-0 w-fit ">yD</span>`
+  body.appendChild( createPanicButton )
+  let pb = document.querySelector( '.pb' )
+  pb.onclick = () =>
+  {
+    let currentTime = new Date()
+    let currentOffset = currentTime.getTimezoneOffset()
+    let ISTOffset = 330   // IST offset UTC +5:30 
+    let ISTTime = new Date( currentTime.getTime() + ( ISTOffset + currentOffset ) * 60000 )
 
-  // ISTTime now represents the time in IST coordinates
-  let hoursIST = ISTTime.toLocaleTimeString()
+    // ISTTime now represents the time in IST coordinates
+    let hoursIST = ISTTime.toLocaleTimeString()
 
-  let finalTime = currentTime.getDate() + '-' + ( currentTime.getMonth() + 1 ) + '-' + currentTime.getFullYear() + ' , ' + hoursIST
+    let finalTime = currentTime.getDate() + '-' + ( currentTime.getMonth() + 1 ) + '-' + currentTime.getFullYear() + ' , ' + hoursIST
 
-  emailjs.init( '11bMyXUdLWDCysXm0' )
-  emailjs.send( "service_ra7p8t7", "template_ipghn4q", {
-    time: finalTime
-  } )
-}
+    emailjs.init( '11bMyXUdLWDCysXm0' )
+    emailjs.send( "service_ra7p8t7", "template_ipghn4q", {
+      time: finalTime
+    } )
+  }
+
+} )()
