4d6e261e77305575df581bddf5fca562814544da
ajt <48979824+ankitjt@users.noreply.github.com>
2023-03-22 16:00:46 +0530

    Add files via upload

--- scripts/commonScripts/addingNavbar_Prompts.js
+++ scripts/commonScripts/addingNavbar_Prompts.js
@@ -7,28 +7,24 @@ let pageUrl = p.substring( 0, p.indexOf( '.' ) )
 
 
 navbarContent.innerHTML = `
-  <div class='text-[10px] tracking-widest font-semibold  text-blue-500 flex items-center justify-between w-full '>
-  <a href="./home.html" class="hover:bg-blue-500 rounded-full">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 hover:text-white" viewBox="0 0 20 20" fill="currentColor">
-            <path fill-rule="evenodd"
-              d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-              clip-rule="evenodd" />
-          </svg>
-        </a>
-        <div class="rightSideNavbar font-semibold flex items-center justify-center">
-        <span class='serverTime mr-1 sm:mr-5'></span>
-          <span class="uppercase">${ pageUrl } </span>
-          <div class="notificationOpener">
+  <div class='tracking-widest font-semibold flex items-center justify-between'>
+ 
+        <div class="rightSideNavbar font-semibold flex items-center justify-between w-full">
+        <span class="uppercase">${ pageUrl } </span>
+        <div class="flex items-center justify-between">
+        <span class='serverTime mr-1'></span>
+          <div class="notificationOpener lg:text-white text-indigo-600">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
-              class="w-4 h-4 ml-1 sm:ml-5 text-red-400 cursor-pointer notification">
+              class="w-4 h-4 ml-1 sm:ml-5 cursor-pointer notification">
               <path stroke-linecap="round" stroke-linejoin="round"
                 d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
             </svg>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
-              class="w-4 h-4 ml-5 hidden text-red-400 openedNotification cursor-pointer">
+              class="w-4 h-4 ml-5 hidden openedNotification cursor-pointer">
               <path stroke-linecap="round" stroke-linejoin="round"
                 d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0M3.124 7.5A8.969 8.969 0 015.292 3m13.416 0a8.969 8.969 0 012.168 4.5" />
             </svg>
+          </div>
           </div>
         </div>
         </div>
--- scripts/commonScripts/prompts.js
+++ scripts/commonScripts/prompts.js
@@ -8,17 +8,17 @@ const promptMessages = ( message, promptType, refresh ) =>
   pageBlocker.classList.remove( 'hidden' )
   promptsWrapper.innerHTML += `
           
-          <section class="prompts flex items-center justify-between md:w-2/3 w-full px-6 py-3 my-1 rounded-lg ease-in-out duration-300 md:hover:bg-gray-800 cursor-pointer ${ promptType === 'error' ? 'bg-rose-600' : 'bg-emerald-600' }">
-            <div class="promptContent font-medium text-xs text-slate-50 w-full flex items-center">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-7 h-7 bg-white text-rose-600 p-1 rounded-lg mr-5 ${ promptType === 'error' ? 'block' : 'hidden' }">
+          <section class="prompts flex items-end justify-between px-6 py-5 my-3 w-full h-fit rounded-xl bg-white shadow-2xl font-semibold text-[10px] uppercase tracking-widest ${ promptType === 'error' ? 'text-rose-600' : 'text-emerald-600' }">
+            <div class="promptContent text-xs w-full flex items-center justify-between">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-7 h-7 bg-white text-rose-600  rounded-lg mr-2 ${ promptType === 'error' ? 'block' : 'hidden' }">
                <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
               </svg>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-7 h-7 bg-white text-emerald-600 p-1 rounded-lg mr-5 ${ promptType === 'success' ? 'block' : 'hidden' }">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-7 h-7 bg-white text-emerald-600  rounded-lg mr-2 ${ promptType === 'success' ? 'block' : 'hidden' }">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
               </svg>
-              <span class='w-4/5'>${ message }</span>
+              <span class='grow text-left'>${ message }</span>
             </div>
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 w-full text-slate-50 closePrompts refreshPage ${ refresh === 'refresh' ? 'refresh' : '' } id='refreshPage' cursor-pointer" viewBox="0 0 20 20"
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 closePrompts refreshPage ${ refresh === 'refresh' ? 'refresh' : '' } id='refreshPage' cursor-pointer" viewBox="0 0 20 20"
               fill="currentColor">
               <path fill-rule="evenodd"
                 d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
